6a105a87ce524e1272d6fa9830826e6b1d50732e
JakSzatan <ddt1@op.pl>
2023-09-19 12:49:36 +0200

    more properties

--- app.vue
+++ app.vue
@@ -17,6 +17,11 @@ useHead({
     },
     { hid: 'og:title', property: 'og:title', content:  'elo' },
     { hid: 'og:description', property: 'og:description', content:  'elo' },
+    { hid: 'og:url', property: 'og:url', content:  'https://media.discordapp.net/attachments/1136207345559539794/1153329446523248741/jakubszatybelko_big_cool_tracktor_400163d3-9bf0-4265-b0c2-5b4099c35401.png?width=1365&height=1365' },
+    { hid: 'twitter:title', property: 'twitter:title', content:  'elo' },
+    { hid: 'twitter:description', property: 'twitter:description', content:  'elo' },
+    { hid: 'twitter:image', property: 'twitter:image', content:  'elo' },
+    { hid: 'twitter:card', property: 'twitter:card', content:  'elo' },
   ],
 });
 </script>
