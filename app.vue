637b3b795a9e2f84ba8d3906c356939f66e232b1
JakSzatan <ddt1@op.pl>
2023-09-19 17:15:20 +0200

    k

--- app.vue
+++ app.vue
@@ -5,26 +5,50 @@
 </template>
 <script setup>
 useHead({
-  title: 'Machine Master',
+  title: theming?.pageTitle || 'Machine Master',
   meta: [
-    {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-    { name: 'charset', content: 'utf-8'},
-    { name: 'description', content: 'elo' },
-    { name: 'keywords', content: 'elo' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { charset: 'utf-8' },
+    {
+      name: 'description',
+      content:
+        theming?.metaDataDescription ||
+        'MachineMaster ist eine Online-Plattform zur Suche und zum Kauf von Landmaschinen und landwirtschaftlichen Geräten. Finden Sie eine Vielzahl von Landmaschinen, Traktoren und Erntemaschinen verschiedener Händler und treten Sie mit dem richtigen Händler für Ihre spezifischen Bedürfnisse in Kontakt. Mit den Suchfilteroptionen können Sie die perfekte Maschine für Ihre Anforderungen finden.',
+    },
+    {
+      name: 'keywords',
+      content:
+        theming?.metaDataTags ||
+        'MachineMaster, Erntemaschinen, landwirtschaftliche Ausrüstung, landwirtschaftliche Maschinen, Händler, Maschinen kaufen, gefilterte Suche, Erntemaschinen kaufen, Mähdrescher, Traktoren, Feldhäcksler, Pflanzmaschinen, Dreschmaschinen, Erntewerkzeuge, Landmaschinenhändler, Landtechnik, Agrartechnik, gebrauchte Erntemaschinen, Neuheiten in der Erntetechnik, landwirtschaftliche Geräte, Anbaugeräte für Erntemaschinen, Ersatzteile für Erntemaschinen, professionelle Erntemaschinen, Hochleistungserntemaschinen, Landwirtschaftliche Maschinenhersteller, Erntemaschinenverleih',
+    },
     {
       hid: 'og:image',
       property: 'og:image',
       content:
-        'https://media.discordapp.net/attachments/1136207345559539794/1153651377483030598/jakubszatybelko_big_cool_tracktor_51cb4751-69c0-46db-8c38-6b2ff00b08cc.png?width=1365&height=1365',
+        theming?.metaDataImage?.url ||
+        'https://media.discordapp.net/attachments/1136207345559539794/1153329446523248741/jakubszatybelko_big_cool_tracktor_400163d3-9bf0-4265-b0c2-5b4099c35401.png?width=1365&height=1365',
     },
-    { hid: 'og:title', property: 'og:title', content:  'elo' },
-    { hid: 'og:description', property: 'og:description', content:  '' },
-    { hid: 'og:type', property: 'og:type', content:  'website'},
-    { hid: 'og:url', property: 'og:url', content:  'https://media.discordapp.net/attachments/1136207345559539794/1153651377483030598/jakubszatybelko_big_cool_tracktor_51cb4751-69c0-46db-8c38-6b2ff00b08cc.png?width=1365&height=1365' },
-    { hid: 'twitter:title', property: 'twitter:title', content:  'elo' },
-    { hid: 'twitter:description', property: 'twitter:description', content:  'elo' },
-    { hid: 'twitter:image', property: 'twitter:image', content:  'elo' },
-    { hid: 'twitter:card', property: 'twitter:card', content:  'elo' },
+    { hid: 'og:title', property: 'og:title', content: theming?.pageTitle || '' },
+    { hid: 'og:description', property: 'og:description', content: theming?.metaDataDescription || '' },
+    { hid: 'og:type', property: 'og:type', content: 'website' },
+    {
+      hid: 'og:url',
+      property: 'og:url',
+      content:
+        theming?.metaDataUrl ||
+        'https://media.discordapp.net/attachments/1136207345559539794/1153329446523248741/jakubszatybelko_big_cool_tracktor_400163d3-9bf0-4265-b0c2-5b4099c35401.png?width=1365&height=1365',
+    },
+    { hid: 'twitter:title', property: 'twitter:title', content: theming?.pageTitle || 'Machine Master' },
+    { hid: 'twitter:description', property: 'twitter:description', content: theming?.metaDataDescription || '' },
+    {
+      hid: 'twitter:image',
+      property: 'twitter:image',
+      content:
+        theming?.metaDataImage?.url ||
+        'https://media.discordapp.net/attachments/1136207345559539794/1153329446523248741/jakubszatybelko_big_cool_tracktor_400163d3-9bf0-4265-b0c2-5b4099c35401.png?width=1365&height=1365',
+    },
+    { hid: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' },
   ],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: theming?.metaDataLogo?.url || '/favicon.svg' }],
 });
 </script>
