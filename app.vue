87c70c7770dac5e9e08737c16ad6ca9424c7af52
JakSzatan <ddt1@op.pl>
2023-09-19 15:40:49 +0200

    test no desc

--- app.vue
+++ app.vue
@@ -16,7 +16,7 @@ useHead({
         'https://media.discordapp.net/attachments/1136207345559539794/1153651377483030598/jakubszatybelko_big_cool_tracktor_51cb4751-69c0-46db-8c38-6b2ff00b08cc.png?width=1365&height=1365',
     },
     { hid: 'og:title', property: 'og:title', content:  'elo' },
-    { hid: 'og:description', property: 'og:description', content:  'elo' },
+    { hid: 'og:description', property: 'og:description', content:  '' },
     { hid: 'og:type', property: 'og:type', content:  'website'},
     { hid: 'og:url', property: 'og:url', content:  'https://media.discordapp.net/attachments/1136207345559539794/1153651377483030598/jakubszatybelko_big_cool_tracktor_51cb4751-69c0-46db-8c38-6b2ff00b08cc.png?width=1365&height=1365' },
     { hid: 'twitter:title', property: 'twitter:title', content:  'elo' },
