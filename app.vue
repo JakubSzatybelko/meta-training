f234069ebeff17e71408888e6041411589bbd124
JakSzatan <ddt1@op.pl>
2023-09-19 18:00:45 +0200

    only head

--- app.vue
+++ app.vue
@@ -42,22 +42,21 @@ useHead({
   ],
   link: [{ rel: 'icon', type: 'image/x-icon', href: theming?.metaDataLogo?.url || '/favicon.svg' }],
 });
-useServerSeoMeta({
-  title: theming?.pageTitle || 'Machine Master',
-  charset: 'utf-8',
-  description:
-    theming?.metaDataDescription ||
-    'MachineMaster ist eine Online-Plattform zur Suche und zum Kauf von Landmaschinen und landwirtschaftlichen Geräten. Finden Sie eine Vielzahl von Landmaschinen, Traktoren und Erntemaschinen verschiedener Händler und treten Sie mit dem richtigen Händler für Ihre spezifischen Bedürfnisse in Kontakt. Mit den Suchfilteroptionen können Sie die perfekte Maschine für Ihre Anforderungen finden.',
-  keywords:
-    theming?.metaDataTags ||
-    'MachineMaster, Erntemaschinen, landwirtschaftliche Ausrüstung, landwirtschaftliche Maschinen, Händler, Maschinen kaufen, gefilterte Suche, Erntemaschinen kaufen, Mähdrescher, Traktoren, Feldhäcksler, Pflanzmaschinen, Dreschmaschinen, Erntewerkzeuge, Landmaschinenhändler, Landtechnik, Agrartechnik, gebrauchte Erntemaschinen, Neuheiten in der Erntetechnik, landwirtschaftliche Geräte, Anbaugeräte für Erntemaschinen, Ersatzteile für Erntemaschinen, professionelle Erntemaschinen, Hochleistungserntemaschinen, Landwirtschaftliche Maschinenhersteller, Erntemaschinenverleih',
-  url: theming?.metaDataUrl || 'https://dev.moerschen.machinemaster.de/',
-  ogImage: theming?.metaDataImage?.url || 'https://postimg.cc/q6kk32wm',
-  ogUrl: theming?.metaDataUrl || 'https://dev.moerschen.machinemaster.de/',
-  ogTitle: theming?.pageTitle || 'Machine Master',
-  ogDescription:
-    theming?.metaDataDescription ||
-    'MachineMaster ist eine Online-Plattform zur Suche und zum Kauf von Landmaschinen und landwirtschaftlichen Geräten. Finden Sie eine Vielzahl von Landmaschinen, Traktoren und Erntemaschinen verschiedener Händler und treten Sie mit dem richtigen Händler für Ihre spezifischen Bedürfnisse in Kontakt. Mit den Suchfilteroptionen können Sie die perfekte Maschine für Ihre Anforderungen finden.',
-
-});
+// useServerSeoMeta({
+//   title: theming?.pageTitle || 'Machine Master',
+//   charset: 'utf-8',
+//   description:
+//     theming?.metaDataDescription ||
+//     'MachineMaster ist eine Online-Plattform zur Suche und zum Kauf von Landmaschinen und landwirtschaftlichen Geräten. Finden Sie eine Vielzahl von Landmaschinen, Traktoren und Erntemaschinen verschiedener Händler und treten Sie mit dem richtigen Händler für Ihre spezifischen Bedürfnisse in Kontakt. Mit den Suchfilteroptionen können Sie die perfekte Maschine für Ihre Anforderungen finden.',
+//   keywords:
+//     theming?.metaDataTags ||
+//     'MachineMaster, Erntemaschinen, landwirtschaftliche Ausrüstung, landwirtschaftliche Maschinen, Händler, Maschinen kaufen, gefilterte Suche, Erntemaschinen kaufen, Mähdrescher, Traktoren, Feldhäcksler, Pflanzmaschinen, Dreschmaschinen, Erntewerkzeuge, Landmaschinenhändler, Landtechnik, Agrartechnik, gebrauchte Erntemaschinen, Neuheiten in der Erntetechnik, landwirtschaftliche Geräte, Anbaugeräte für Erntemaschinen, Ersatzteile für Erntemaschinen, professionelle Erntemaschinen, Hochleistungserntemaschinen, Landwirtschaftliche Maschinenhersteller, Erntemaschinenverleih',
+//   url: theming?.metaDataUrl || 'https://dev.moerschen.machinemaster.de/',
+//   ogImage: theming?.metaDataImage?.url || 'https://postimg.cc/q6kk32wm',
+//   ogUrl: theming?.metaDataUrl || 'https://dev.moerschen.machinemaster.de/',
+//   ogTitle: theming?.pageTitle || 'Machine Master',
+//   ogDescription:
+//     theming?.metaDataDescription ||
+//     'MachineMaster ist eine Online-Plattform zur Suche und zum Kauf von Landmaschinen und landwirtschaftlichen Geräten. Finden Sie eine Vielzahl von Landmaschinen, Traktoren und Erntemaschinen verschiedener Händler und treten Sie mit dem richtigen Händler für Ihre spezifischen Bedürfnisse in Kontakt. Mit den Suchfilteroptionen können Sie die perfekte Maschine für Ihre Anforderungen finden.',
+// });
 </script>
