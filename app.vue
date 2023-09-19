6f8ec545058415046d49a0721cbc2a39264fb97b
JakSzatan <ddt1@op.pl>
2023-09-19 17:48:18 +0200

    add it outside

--- app.vue
+++ app.vue
@@ -15,16 +15,12 @@ useServerSeoMeta({
     theming?.metaDataTags ||
     'MachineMaster, Erntemaschinen, landwirtschaftliche Ausrüstung, landwirtschaftliche Maschinen, Händler, Maschinen kaufen, gefilterte Suche, Erntemaschinen kaufen, Mähdrescher, Traktoren, Feldhäcksler, Pflanzmaschinen, Dreschmaschinen, Erntewerkzeuge, Landmaschinenhändler, Landtechnik, Agrartechnik, gebrauchte Erntemaschinen, Neuheiten in der Erntetechnik, landwirtschaftliche Geräte, Anbaugeräte für Erntemaschinen, Ersatzteile für Erntemaschinen, professionelle Erntemaschinen, Hochleistungserntemaschinen, Landwirtschaftliche Maschinenhersteller, Erntemaschinenverleih',
   url: theming?.metaDataUrl || 'https://dev.moerschen.machinemaster.de/',
-  og: {
-    title: theming?.pageTitle || 'Machine Master',
-    description:
-      theming?.metaDataDescription ||
-      'MachineMaster ist eine Online-Plattform zur Suche und zum Kauf von Landmaschinen und landwirtschaftlichen Geräten. Finden Sie eine Vielzahl von Landmaschinen, Traktoren und Erntemaschinen verschiedener Händler und treten Sie mit dem richtigen Händler für Ihre spezifischen Bedürfnisse in Kontakt. Mit den Suchfilteroptionen können Sie die perfekte Maschine für Ihre Anforderungen finden.',
-    type: 'website',
-    url: theming?.metaDataUrl || 'https://dev.moerschen.machinemaster.de/',
-    image:
-      theming?.metaDataImage?.url ||
-      'https://media.discordapp.net/attachments/1136207345559539794/1153329446523248741/jakubszatybelko_big_cool_tracktor_400163d3-9bf0-4265-b0c2-5b4099c35401.png?width=1365&height=1365',
-  },
+  ogImage: theming?.metaDataImage?.url || 'https://media.discordapp.net/attachments/1136207345559539794/1153329446523248741/jakubszatybelko_big_cool_tracktor_400163d3-9bf0-4265-b0c2-5b4099c35401.png?width=1365&height=1365',
+  ogUrl: theming?.metaDataUrl || 'https://dev.moerschen.machinemaster.de/',
+  ogTitle: theming?.pageTitle || 'Machine Master',
+  ogDescription:
+    theming?.metaDataDescription ||
+    'MachineMaster ist eine Online-Plattform zur Suche und zum Kauf von Landmaschinen und landwirtschaftlichen Geräten. Finden Sie eine Vielzahl von Landmaschinen, Traktoren und Erntemaschinen verschiedener Händler und treten Sie mit dem richtigen Händler für Ihre spezifischen Bedürfnisse in Kontakt. Mit den Suchfilteroptionen können Sie die perfekte Maschine für Ihre Anforderungen finden.',
+  
 });
 </script>
